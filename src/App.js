7e7422d5cbce175c6cccbc2db37c0849d9bc47fe
Chanson9892 <chan.hanson5@gmail.com>
2021-02-15 11:42:30 -0800

    added a contact section and about me section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className='title'>Hello! I am Chandler Hanson! I am a Software Engineer</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,23 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className='aboutDiv'>
+        <h3>About Me</h3>
+        <p>Hello, my name is Chandler Hanson and I am from Sammamish, WA. 
+          I recently I graduated from University of Washington with a Bachelor of Science in Earth and Space Science 
+          with a focus in physics and a Minor in Physics. Once COVID-19 lockdown protocols happened I took a hard look 
+          about what I want to do with my life. I looked at my university courses and noticed that my favorite classes 
+          had a programming aspect to them. The feeling of getting your code to work is a feeling like no other. 
+          I wanted to pursue that feeling and make a career out of it. This led me to Flatiron School Software Engineering 
+          Bootcamp where I learned web development skills and languages such as Ruby, Ruby on Rails, JavaScript, and React.js 
+          to make full-fledged web applications. When I am not coding, I like to bake, read, play videogames, and play basketball. </p>
+          <br></br>
+      </div>
+      <div className='contactDiv'>
+        <h2>Contact Me</h2>
+        <p>Feel free to contact me via LinkedIn or my email.</p>
+      </div>
     </div>
   );
 }
