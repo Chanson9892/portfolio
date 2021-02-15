9568b640c87da2bd8741844f27c8a323dc29f1c8
Chanson9892 <chan.hanson5@gmail.com>
2021-02-15 12:18:07 -0800

    added linkedin and github links as their icons

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           had a programming aspect to them. The feeling of getting your code to work is a feeling like no other. 
           I wanted to pursue that feeling and make a career out of it. This led me to Flatiron School Software Engineering 
           Bootcamp where I learned web development skills and languages such as Ruby, Ruby on Rails, JavaScript, and React.js 
-          to make full-fledged web applications. When I am not coding, I like to bake, read, play videogames, and play basketball. </p>
+          to make full-fledged web applications! When I am not coding, I like to bake, read, play videogames, and play basketball. </p>
           <br></br>
       </div>
       <div className='resumeDiv'>
@@ -38,6 +38,14 @@ function App() {
       <div className='contactDiv'>
         <h2>Contact Me</h2>
         <p>Feel free to contact me via LinkedIn or my email.</p>
+        <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/chandler-hanson/'>
+        <img  alt='Chandler Hanson LinkedIn' width='22px' src='https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg' />
+        </a>
+        <br></br>
+        <a target="_blank" rel="noreferrer" href='https://github.com/Chanson9892'>
+        <img  alt='Chandler Hanson Github' width='22px' src='https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/github.svg' /> 
+        </a>
+
       </div>
     </div>
   );
